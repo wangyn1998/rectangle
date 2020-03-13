eafea16c0c5645caa0b4e330a0b17df39e0886b1
wangyn <928069834@qq,com>
2020-03-13 09:47:54 +0800

    wyn

--- rectangle.js
+++ rectangle.js
@@ -2,6 +2,7 @@ function roundFractional(x, n) {
     return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
 }
 $(function(){
+    //get dom elem
     var $width = $('#width'),
         $height = $('#height'),
         $btnCal = $('#calculate'),
@@ -9,15 +10,59 @@ $(function(){
         $area = $('#area');
     /*calc button click event*/
     $btnCal.click(function(){
+        //validate if error return;
+        if(!validate('#width') || !validate('#height')) return;
+
         //get value
-        var w = $width.val(),       
-            h = $height.val();
+        var w = Number($width.val()),
+            h = Number($height.val());
         //calculate
-        // var p = roundFractional(w*2 + h*2, 4);
-        //     a = roundFractional(w * h, 10);
+        var p = roundFractional(w*2 + h*2, 4);
+            a = roundFractional(w * h, 10);
         //output
-        var r = new Rectangle(w, h);
-        $perimeter.val(r.perimeter());
-        $area.val(r.area());
+        $perimeter.val(p);
+        $area.val(a);
+    });
+
+    $width.focusout(function(){
+        //if(!validat(width))select this;
+        if(!validate('#width')) $width.select();
+    });
+
+    $height.focusout(function(){
+        //if(!validat(height))select this;
+        if(!validate('#height')) $height.select();
     })
+
+    function validate(field){
+        //get DOM error message
+        var $data = $(field),
+            $msg = $(field + '-validation-message');
+
+        //validate null
+        if($data.val() === ''){
+            $msg.html('不能为空！')
+            $data.select();
+            return false
+        }
+
+        //validate number
+        if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())){
+            $msg.html('必须是数值！')
+            $data.select();
+            return false
+        }
+
+        //validate > 0
+        if(Number($data.val()) < 0){
+            $msg.html('必须大于零！')
+            $data.select();
+            return false
+        }
+
+            //prompt error message
+            //return false;
+        
+        return true;
+    }
 });
